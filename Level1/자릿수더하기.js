c210094e7c34904c8924a4a2d01eedeb5b6cfcf2
Anakin787 <seonbi1998@naver.com>
2023-08-10 20:45:44 +0900

    자릿수더하기

--- Level1/자릿수더하기.js
+++ Level1/자릿수더하기.js
@@ -2,10 +2,11 @@ function solution(n)
 {
     const tmp = n.toString()
     const splitArr = tmp.split("")
-    const answer = 0
+    let answer = 0
     for(let i = 0;i < splitArr.length; i++){
-        answer += splitArr[i]
+        answer += Number(splitArr[i])
     }
+        
     return answer;
 }
 
@@ -15,3 +16,27 @@ function solution(n)
 
 제한사항
 N의 범위 : 100,000,000 이하의 자연수 */
+
+/* speed test
+테스트 1 〉	통과 (0.04ms, 33.5MB)
+테스트 2 〉	통과 (0.06ms, 33.5MB)
+테스트 3 〉	통과 (0.04ms, 33.5MB)
+테스트 4 〉	통과 (0.04ms, 33.5MB)
+테스트 5 〉	통과 (0.04ms, 33.6MB)
+테스트 6 〉	통과 (0.06ms, 33.5MB)
+테스트 7 〉	통과 (0.04ms, 33.4MB)
+테스트 8 〉	통과 (0.05ms, 33.5MB)
+테스트 9 〉	통과 (0.05ms, 33.4MB)
+테스트 10 〉통과 (0.04ms, 33.5MB)
+테스트 11 〉통과 (0.07ms, 33.4MB)
+테스트 12 〉통과 (0.06ms, 33.5MB)
+테스트 13 〉통과 (0.07ms, 33.6MB)
+테스트 14 〉통과 (0.04ms, 33.5MB)
+테스트 15 〉통과 (0.04ms, 33.5MB)
+테스트 16 〉통과 (0.06ms, 33.5MB)
+테스트 17 〉통과 (0.04ms, 33.5MB)
+테스트 18 〉통과 (0.06ms, 33.5MB)
+테스트 19 〉통과 (0.04ms, 33.5MB)
+테스트 20 〉통과 (0.04ms, 33.5MB)
+테스트 21 〉통과 (0.04ms, 33.4MB)
+*/
